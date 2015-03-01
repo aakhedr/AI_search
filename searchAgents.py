1e5f7d09b5ffd73288d760d6bafcdf9c8c4d5df0
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2015-03-01 05:02:46 +0200

    More optimisation q7

--- searchAgents.py
+++ searchAgents.py
@@ -475,31 +475,29 @@ def foodHeuristic(state, problem):
     foodDots = foodGrid.asList()
     dotToPos = []
     for dot in foodDots:
-        # mazeDistance (better score only)
-        if (dot, position) in problem.heuristicInfo.keys():
+        # mazeDistance with caching in heuristicInfo dict
+        if dot == position:
+            dotToPos.append(0)
+        elif (dot, position) in problem.heuristicInfo.keys():
             dotToPos.append(problem.heuristicInfo[(dot, position)])
         else:
             dist = mazeDistance(position, dot, problem.startingGameState)
             dotToPos.append(dist)
             problem.heuristicInfo[(dot, position)] = dist
-        # Manhattan distance
-        # dotToPos.append(util.manhattanDistance(position, dot))
 
     dotToDot = []
-    for dot1 in foodDots:
-        for dot2 in foodDots:
-            # mazeDistance (better score only)
-            if (dot1, dot2) in problem.heuristicInfo.keys():
-                dotToDot.append(problem.heuristicInfo[(dot1, dot2)])
-            elif (dot2, dot1) in problem.heuristicInfo.keys():
-                dotToDot.append(problem.heuristicInfo[(dot2, dot1)])
-            else:
-                dist = mazeDistance(dot1, dot2, problem.startingGameState)
-                dotToDot.append(dist)
-                problem.heuristicInfo[(dot1, dot2)] = dist
-
-            # Manhattan distance
-            # dotToDot.append(util.manhattanDistance(dot1, dot2))
+    for i in range(len(foodDots)):
+        for j in range(len(foodDots)):
+            if i == j:
+                dotToDot.append(0)                
+            elif i > j:
+                # mazeDistance with caching in heuristicInfo dict
+                if (foodDots[i], foodDots[j]) in problem.heuristicInfo.keys():
+                    dotToDot.append(problem.heuristicInfo[(foodDots[i], foodDots[j])])
+                else:
+                    dist = mazeDistance(foodDots[i], foodDots[j], problem.startingGameState)
+                    dotToDot.append(dist)
+                    problem.heuristicInfo[(foodDots[i], foodDots[j])] = dist
 
     if len(dotToPos) + len(dotToDot) == 0:
         return 0
